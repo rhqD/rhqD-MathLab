ee2497ae7313e9b4bb167d7b3b82c08f360a6e0a
Hanquan Ren <hanquan.ren@CNhanquan.ren>
2019-10-28 14:15:36 +0800

    enable 3d graph

--- sample/app/index.js
+++ sample/app/index.js
@@ -29,9 +29,6 @@ window.onload = () => {
   worker.onmessage = (event) => {
     const {img, points, finished} = event.data;
     ctx.putImageData(img, 0, 0);
-    if (!finished){
-      return;
-    }
     const data = [{
       z: points,
       type: 'surface',
